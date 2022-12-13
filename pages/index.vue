5f32428545d7cdeee57a9048dc03d321bf37e968
midlantica <druharper@gmail.com>
2022-12-13 09:23:16 -0600

    save

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,3 @@
 <template>
-  <h1 class="text-green-500 ">Hello, world!!!</h1>
+  <h1 class="text-green-500 bg-green-900">Hello, world!!!!!</h1>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  css: ['@/assets/css/main.css'],
   content: [
-    "@/components/**/*.{js,vue,ts}",
-    "@/layouts/**/*.vue",
-    "@/pages/**/*.vue",
-    "@/plugins/**/*.{js,ts}",
-    "@/nuxt.config.{js,ts}",
-    "@/app.vue",
-    "@/error.vue",
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",
   ],
   theme: {
     extend: {},
